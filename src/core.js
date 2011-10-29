4d9d94b5b276052ed2f3344f3ba1adec16b9f599
Gorbach Alexey <gorbach.alexey@gmail.com>
2011-10-29 12:44:10 +0300

    variance method fixed. we should divide by length of the array, not by length - 1.

--- src/core.js
+++ src/core.js
@@ -550,7 +550,7 @@ jStat.extend({
 		for( ; i >= 0; i-- ) {
 			stSum += Math.pow(( arr[i] - mean ), 2 );
 		}
-		return stSum / ( arr.length - 1 );
+		return stSum / arr.length;
 	},
 
 	// standard deviation of an array
